cbb0166d5cb4c5fdc1b578d870bb885d0e78a617
dapevi1997 <dapevi97@gmail.com>
2023-01-11 16:16:35 -0500

    Kata6Test ok

--- src/main/java/katas/Kata6.java
+++ src/main/java/katas/Kata6.java
@@ -4,6 +4,8 @@ import model.Movie;
 import util.DataUtil;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 /*
     Goal: Retrieve the url of the largest boxart using map() and reduce()
@@ -13,7 +15,14 @@ import java.util.List;
 public class Kata6 {
     public static String execute() {
         List<Movie> movies = DataUtil.getMovies();
+        Optional<String> resultMovies = movies.stream().map(movie -> movie.getBoxarts()).flatMap(boxArts -> boxArts.stream()).reduce((largest, element)->{
+            if (element.getUrl().length() > largest.getUrl().length()) {
+                return element;
+            }else {
+                return largest;
+            }
+        }).map(boxArt -> boxArt.getUrl());
 
-        return "someUrl";
+        return resultMovies.orElse("");
     }
 }
