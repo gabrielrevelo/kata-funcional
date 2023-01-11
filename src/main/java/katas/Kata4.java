8fa3a75dc88dd839430df9d69448b7d1cdf5629f
dapevi1997 <dapevi97@gmail.com>
2023-01-11 15:18:12 -0500

    Kata4Test ok

--- src/main/java/katas/Kata4.java
+++ src/main/java/katas/Kata4.java
@@ -8,6 +8,7 @@ import util.DataUtil;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
     Goal: Retrieve id, title, and a 150x200 box art url for every video
@@ -17,7 +18,10 @@ import java.util.Map;
 public class Kata4 {
     public static List<Map> execute() {
         List<MovieList> movieLists = DataUtil.getMovieLists();
+        List<Map> resultMovieList = movieLists.stream().map(movieList -> movieList.getVideos()).flatMap(movies -> movies.stream())
+                .map(movie -> ImmutableMap.of("id", movie.getId(), "title", movie.getTitle(), "boxart",movie.getBoxarts()))
+                .collect(Collectors.toList());
 
-        return ImmutableList.of(ImmutableMap.of("id", 5, "title", "Bad Boys", "boxart", new BoxArt(150, 200, "url")));
+        return resultMovieList;
     }
 }
