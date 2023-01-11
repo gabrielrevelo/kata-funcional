653ddd837c929dc8e0f149bf4b5c97b4205ec534
dapevi1997 <dapevi97@gmail.com>
2023-01-11 15:08:19 -0500

    Kata3Test ok

--- src/main/java/katas/Kata3.java
+++ src/main/java/katas/Kata3.java
@@ -6,6 +6,7 @@ import model.MovieList;
 import util.DataUtil;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /*
     Goal: Use map() and flatMap() to project and flatten the movieLists into an array of video ids (flatMap(c -> c.stream()))
@@ -15,7 +16,9 @@ import java.util.List;
 public class Kata3 {
     public static List<Integer> execute() {
         List<MovieList> movieLists = DataUtil.getMovieLists();
+        var resultMovieList = movieLists.stream().map(movieList -> movieList.getVideos()).flatMap(movies -> movies.stream())
+                .map(movie -> movie.getId()).collect(Collectors.toList());
 
-        return ImmutableList.of(1, 2, 3);
+        return resultMovieList;
     }
 }
