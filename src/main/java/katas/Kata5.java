7e63e2d1812c4492579c10c18f8918503f26f224
dapevi1997 <dapevi97@gmail.com>
2023-01-11 15:52:16 -0500

    Kata5Test ok

--- src/main/java/katas/Kata5.java
+++ src/main/java/katas/Kata5.java
@@ -1,14 +1,10 @@
 package katas;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
-import model.BoxArt;
 import model.Movie;
-import model.MovieList;
 import util.DataUtil;
 
 import java.util.List;
-import java.util.Map;
+import java.util.Optional;
 
 /*
     Goal: Retrieve the largest rating using reduce()
@@ -18,7 +14,16 @@ import java.util.Map;
 public class Kata5 {
     public static Double execute() {
         List<Movie> movies = DataUtil.getMovies();
+        Optional<Double> resultMovies = movies.stream().reduce((rating, movie)-> {
+            if (movie.getRating() > rating.getRating()) {
+                return movie;
+            }else {
+                return rating;
+            }
+        }).map(movie -> movie.getRating());
 
-        return 3.0;
+
+        return resultMovies.orElse(0.0);
+
     }
 }
